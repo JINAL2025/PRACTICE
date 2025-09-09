4b2722afddffb61fa3ed3a434fe8a04586dc47eb
JINAL2025 <chetanpandya2011@gmail.com>
2025-09-09 17:31:03 +0530

    Create Print479.c

---
+++ Print479.c
@@ -0,0 +1,19 @@
+//Accept 10 values and print the 4th, 7th and 9th ones
+#include <stdio.h>
+void main()
+{
+int a[10], i;
+printf("Enter 10 numbers : ");
+for(i=0;i<10;i++)
+{
+scanf("%d", &a[i]);
+}
+printf("The 4th, 7th and 9th elements are :\n");
+for(i=0;i<10;i++)
+{
+if(i==3||i==6||i==8)
+{
+printf("%d  ", a[i]);
+}
+}
+}
